a201de1c92b9647dc53fdf0be02360256cf2a2b1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2017-04-28 08:52:14 +0000

    Rename module "hello" with "response"

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ extern crate iron;
 use iron::prelude::*;
 use iron::status;
 
-mod hello;
+mod response;
 mod config;
 
 use config::ServerConfiguration;
 
 fn main() {
     fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, hello::hello())))
+        Ok(Response::with((status::Ok, response::hello())))
     }
 
     let mut config = ServerConfiguration::new();
