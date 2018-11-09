53f4180a27b28cf7d7ba10e99affa1fcaacb7418
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-09 18:25:54 +0900

    Add provision command for development

---
+++ misc/provision.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install build-essential rake bison git gperf automake m4 \
+                autoconf libtool cmake pkg-config libcunit1-dev ragel libprocps4-dev
+sudo apt-get -y upgrade
+
+curl https://sh.rustup.rs -sSf > rustup.sh
+sh rustup.sh -y
+source $HOME/.cargo/env
+
+git clone https://github.com/matsumotory/k2i
+cd k2i && cargo build
