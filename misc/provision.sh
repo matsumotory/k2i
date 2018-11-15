83bec3eef92e6d26930c0fe08d078d133a7581db
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-15 14:39:43 +0900

    Remove nano package in development environment

--- misc/provision.sh
+++ misc/provision.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get -y install build-essential rake bison git gperf automake m4 \
                 autoconf libtool cmake pkg-config libcunit1-dev ragel libprocps4-dev
+sudo apt-get -y remove nano
 
 curl https://sh.rustup.rs -sSf > rustup.sh
 sh rustup.sh -y
